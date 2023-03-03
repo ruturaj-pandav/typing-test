adad4596affdb51ab9f00ef5e2a1d41d07f74c2b
rutu-pandav <ruturajpandav@outlook.com>
2023-03-03 13:32:17 +0530

    minor changes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,7 +3,7 @@ export default function Timer({seconds}) {
     
     return (
         <div className="text-white my-8" >
-            <span className='block text-3xl my-3 font-bold '>Starting in </span>
+            <span className='block text-3xl my-3 font-bold '>Test starting in </span>
             <span className='block text-2xl '>{5-seconds} seconds</span>
         </div>
     )
